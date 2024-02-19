5c49935c539e38f2fe1c3d7a791062c10cb9fbf6
Mindula Madhuhansa <76749525+mindula-madhuhansa@users.noreply.github.com>
2024-02-19 17:53:28 +0530

    add project

---
+++ components/ThemeProvider.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { ThemeProvider } from "next-themes";
+
+function Providers({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ThemeProvider enableSystem={true} attribute="class">
+      {children}
+    </ThemeProvider>
+  );
+}
+
+export default Providers;
